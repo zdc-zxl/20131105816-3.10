5ff1d792bdb7953db3b42fcaf24a8363ee34ba68
zdc-zxl <dllm@Mac19.local>
2016-03-10 11:01:52 +0800

    for if while switch

--- 20131105816 3.10/main.swift
+++ 20131105816 3.10/main.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 
+//xun huan
 for var i in 1...10{
     print("\(i),")
 }
@@ -18,7 +19,51 @@ for var j=0;j<5;j++ {
 }
 
 var a=0
+//while xunhuan
 while (a<5) {
     print("a=\(a)");
     a++
 }
+
+var a1=1
+// do while yu ju
+repeat {
+    print("a1=\(a1)");
+    a1=a1+1
+}
+
+    while(
+        a1<3
+)
+//pan duan
+var  days=20
+if days<=7{
+    print("一个星期")
+}
+else if days<16 {
+    print("一个多星期")
+}
+else {
+    print("两个多星期")
+}
+
+var k = 4
+switch k {
+case 0:
+     print("0")
+    case 1:
+    print("1")
+default:
+    print("3")
+}
+
+var e = 0
+switch e {
+case 0:
+    print ("0") //被输出
+case 1:
+    "1"
+default:
+    "2"
+}
+
